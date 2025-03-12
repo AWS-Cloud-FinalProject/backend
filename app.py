3fd2525682372c97f71dda0fd91a3936d860d968
devdoyeon <doyeonyou@naver.com>
2025-03-13 00:23:42 +0900

    updated

--- app.py
+++ app.py
@@ -2,15 +2,20 @@ from fastapi import FastAPI, HTTPException, Header
 from routers import user, todo, diary
 from dotenv import load_dotenv
 from routers.cognito import cognito_client, CLIENT_ID
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 app = FastAPI()
-'''
-1. 유저
-2. 일기
-3. 투두
-'''
+
+# CORS 미들웨어 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 도메인에서 접근을 허용
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+)
 
 # 서버 상태 확인
 @app.get("/ping")
